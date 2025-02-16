a32ae1a3e6537c36142b52d32dc14dccbb9f4986
Stacy York <stacyyyork@gmail.com>
2025-02-16 17:45:45 +0200

    completed task 5 and 6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,31 @@
+// встановлення placeholder при input.active
 
+const input = document.getElementById('myInput');
+
+input.addEventListener('focus', function() {
+    input.setAttribute('placeholder', 'Type area');
+});
+
+input.addEventListener('blur', function() {
+    input.removeAttribute('placeholder');
+});
+
+
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    if (!email || !password) {
+    alert('All form fields must be filled in');
+        return; 
+    }
+    const formData = {
+    email: email,
+    password: password
+    };
+    console.log(formData);
+    form.reset();
+});
